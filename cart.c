aaa5754d7e5c60cbc3cc0883eb0bacf007e35b95
shii-ro <wesley.7.castro@gmail.com>
2021-12-21 20:17:07 -0300

    minor fixes and added basic MMC1 support

--- cart.c
+++ cart.c
@@ -4,6 +4,8 @@
 #include "common.h"
 #include "nes.h"
 #include "cart.h"
+#include "mappers/mapper00.c"
+#include "mappers/mapper01.c"
 
 void cart_load_header(struct nes *nes, FILE *rom )
 {
@@ -17,19 +19,40 @@ void cart_load_header(struct nes *nes, FILE *rom )
     nes->cart.chr_rom_size = nes->cart.header.chr_rom_size * (8 * 1024);
     nes->cart.prg_rom_size = nes->cart.header.prg_rom_size * (16 * 1024);
 
-    printf("CHR SIZE: %d PRG_SIZE %d\n", nes->cart.chr_rom_size, nes->cart.prg_rom_size);
-    printf("MIRORING: %x ", nes->cart.header.flags_6 & 0x1);
+    if (!nes->cart.header.chr_rom_size)
+        nes->mapper.uses_chr_ram = true;
+    if (nes->cart.header.flags_6 & 2)
+        nes->mapper.uses_prg_ram = true;
 
-    nes->cart.mapper_index = (nes->cart.header.flags_7 & 0xFF) << 8 | ((nes->cart.header.flags_6 >> 8) & 0xFF);
+    printf("CHR SIZE: %d PRG_SIZE %d\n", nes->cart.chr_rom_size, nes->cart.prg_rom_size);
+    printf("MIRRORING: %x ", nes->cart.header.flags_6 & 0x1);
+
+    nes->cart.mapper_index = (nes->cart.header.flags_7 & 0xFF) << 4 | ((nes->cart.header.flags_6 >> 4) & 0xFF);
+    printf("MAPPER: %x\n", nes->cart.mapper_index);
 }
 
 void cart_load_rom(struct nes *nes, FILE *rom)
 {
-    nes->cart.chr_rom = malloc(nes->cart.chr_rom_size);
+    
     nes->cart.prg_rom = malloc(nes->cart.prg_rom_size);
 
     fread(nes->cart.prg_rom, sizeof(u8), nes->cart.prg_rom_size, rom);
-    fread(nes->cart.chr_rom, sizeof(u8), nes->cart.chr_rom_size, rom);
+    nes->cart.prg_rom_banks = (struct prg_rom_banks *) &nes->cart.prg_rom;
+    if (nes->mapper.uses_chr_ram)
+    {
+        printf("uUSES CHR RAM\n");
+        nes->cart.chr_rom = malloc(sizeof(u8) * 0x2000);
+    }
+    else
+    {
+        nes->cart.chr_rom = malloc(nes->cart.chr_rom_size);
+        fread(nes->cart.chr_rom, sizeof(u8), nes->cart.chr_rom_size, rom);
+    }
+
+    if (nes->mapper.uses_prg_ram || true){
+        printf("Uses PRG RAM\n");
+        nes->mapper.prg_ram = malloc(sizeof(u8) * 0x2000);
+    }
 }
 
 void cart_init(struct nes *nes, char *romname)
@@ -44,25 +67,10 @@ void cart_init(struct nes *nes, char *romname)
 
 void cart_close(struct nes *nes)
 {
+    if (nes->mapper.uses_prg_ram || true)
+        free(nes->mapper.prg_ram);
     free(nes->cart.prg_rom);
     free(nes->cart.chr_rom);
-}
-
-u8 mapper00_read(struct mapper *mapper, u16 addr)
-{
-    // reading from  C000 and beyond always have the 15th bit true
-    return mapper->prg_rom_bank[(addr >> 14) & 0x1][addr & 0x3FFF];
-}
-
-void mapper00_init(struct nes *nes)
-{
-    
-    nes->cart.mapper.prg_rom_bank[0] = nes->cart.prg_rom;
-    nes->cart.mapper.prg_rom_bank[1] = ( nes->cart.header.prg_rom_size == 1) ?
-                                    nes->cart.prg_rom : 
-                                    &nes->cart.prg_rom[0x4000];
-
-    nes->cart.mapper.mapper_read = &mapper00_read;
 }
 
 void mapper_init(struct nes *nes)
@@ -70,6 +78,7 @@ void mapper_init(struct nes *nes)
     switch (nes->cart.mapper_index)
     {
     case 0x00: mapper00_init(nes); break;
+    case 0x01: mapper01_init(nes); break;
     default:
         printf("Mapper not implemented: %02x\n", nes->cart.mapper_index);
     }
